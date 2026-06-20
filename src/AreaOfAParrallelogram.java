fc7155712430e3fa1199dbb48de437b5356a2818
d4rkn37 <edgarsaid586@gmail.com>
2026-06-20 17:32:57 +0300

    java static initialiser

---
+++ src/AreaOfAParrallelogram.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class AreaOfAParrallelogram {
+    static int B;
+    static int H;
+    static boolean flag;
+
+    static{
+        Scanner sc = new Scanner(System.in);
+        B =  sc.nextInt();
+        H =  sc.nextInt();
+
+        if ( B <= 0 || H <= 0) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }else if (B > 100 || H > 100) {
+            flag = false;
+        } else  {
+            flag = true;
+        }
+    }
+
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }
+
+}
+
